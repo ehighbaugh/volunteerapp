07ccc18177a95dd4fc2dc3d34461170b5ab5cd72
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-06-16 09:50:14 -0400

    init commit

---
+++ VTApp/Models/Event.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VTApp.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public String Date { get; set; }
+        public String StartTime { get; set; }
+        public String EndTime { get; set; }
+
+        public ICollection<Volunteer> Volunteers { get; set; }
+        public ICollection<Organization> Organizations { get; set; }
+    }
+}
